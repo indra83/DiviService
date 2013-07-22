30f7bcfc0832f0ccba2ede87d6ab5028fc7755ca
Subhash Chandra <TMaYaD@LoonyB.in>
2013-07-22 18:42:19 +0530

    added v1 route to the login action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 DiviService::Application.routes.draw do
   resource :session
+
+  # V1 routes
+  namespace :v1 do
+    post "loginUser", to: 'sessions#create', as: :login, defaults: { format: 'json' }
+  end
 end
