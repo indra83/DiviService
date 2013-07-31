bf7efd03ce6333193c81c532567bf673ebf9a733
Subhash Chandra <TMaYaD@LoonyB.in>
2013-07-31 06:13:31 +0530

    added filepicker support for updates

--- app/admin/update.rb
+++ app/admin/update.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Update do
 
+  form do |f|
+    filepicker_js_include_tag
+    f.inputs "Details" do
+      f.input :book
+      f.input :description
+      f.input :details
+    end
+    f.inputs "File Upload" do
+      f.input :file, as: :filepicker
+    end
+    f.actions
+  end
 end
