047dd3b340d9d0a538675f81aaada719db118207
Subhash Chandra <TMaYaD@LoonyB.in>
2013-08-02 20:53:22 +0530

    fixed filepicker

--- app/inputs/filepicker_input.rb
+++ app/inputs/filepicker_input.rb
@@ -1,5 +1,5 @@
 class FilepickerInput < Formtastic::Inputs::StringInput
   def input_html_options
-    super.merge 'type' => 'filepicker-dragdrop', 'data-fp-key' => ::Rails.application.config.filepicker_rails.api_key
+    super.merge 'type' => 'filepicker-dragdrop', 'data-fp-apikey' => ::Rails.application.config.filepicker_rails.api_key
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Load heroku vars from local file
+heroku_env = File.expand_path('../heroku_env.rb', __FILE__)
+
+load heroku_env if File.exists? heroku_env
