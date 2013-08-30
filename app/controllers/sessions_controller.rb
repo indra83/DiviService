40318686331e730f9f252cd5f70b5231d2e48849
Subhash Chandra <TMaYaD@LoonyB.in>
2013-08-30 17:21:38 +0530

    accepting string for user id during login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    @current_user = User.find params[:uid]
+    @current_user = User.find params[:uid].to_i
     if @current_user && @current_user.authenticate(params[:password])
       session[:current_user_id] = @current_user.id
     else
