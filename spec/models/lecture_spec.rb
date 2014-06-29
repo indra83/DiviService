82239e6f57b0254d1638a1b61233fe2eff20c405
Subhash Chandra <TMaYaD@LoonyB.in>
2014-06-29 02:26:34 -0400

    bug fixes in specs

--- spec/models/lecture_spec.rb
+++ spec/models/lecture_spec.rb
@@ -2,12 +2,7 @@ require 'spec_helper'
 
 describe Lecture do
   subject(:lecture) { create :lecture }
-  its(:status) { should == 'live' }
-
-  it "should autoexpire in one hour" do
-    lecture.start_time = 2.hours.ago
-    lecture.computed_status.should == 'expired'
-  end
+  its(:status) { should == :live }
 
   it "should check if any lectures are live" do
     lecture.save
