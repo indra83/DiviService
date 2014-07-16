be25c2e1b4a0587cf1becd36c787429e78fe42ff
Subhash Chandra <TMaYaD@LoonyB.in>
2014-07-17 00:11:23 +0530

    fixed the issue where user is not being assigned a new tablet

--- app/controllers/tablets_controller.rb
+++ app/controllers/tablets_controller.rb
@@ -5,7 +5,10 @@ class TabletsController < ApplicationController
   def create
     tablet = Tablet.where(device_id: tablet_params.delete(:device_id)).first_or_initialize
     status = tablet.persisted? ? 'updated' : 'created'
-    tablet.update_attributes(tablet_params.merge(user_id: current_user.id))
+    tablet.assign_attributes(tablet_params)
+    current_user.tablet = tablet
+    # Optionally save. But due to the weird implementation of has_one this is already done in the above assignment
+    # tablet.save
 
     if tablet.errors.present?
       render json: {error: {code:422, message: "The tablet can not be #{status} due to validation errors", errors: tablet.errors} }
