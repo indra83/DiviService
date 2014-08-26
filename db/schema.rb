ca908c073cf5a31e1b56cd62fc2c7931cc6318b8
Subhash Chandra <TMaYaD@LoonyB.in>
2014-08-26 17:23:42 +0530

    exposed class rooms members via api

--- db/schema.rb
+++ db/schema.rb
@@ -210,13 +210,13 @@ ActiveRecord::Schema.define(version: 20140826071747) do
 
   create_table "users", force: true do |t|
     t.string   "name"
+    t.string   "password_digest"
     t.string   "role"
+    t.string   "token"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "pic"
     t.string   "metadata"
-    t.string   "token"
-    t.string   "password_digest"
     t.string   "phone"
     t.string   "email"
     t.string   "parent_phone"
