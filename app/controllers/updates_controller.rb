96d80511051e2e4abddf7a89d6c5f352417c00b3
Subhash Chandra <TMaYaD@LoonyB.in>
2014-08-29 16:04:25 +0530

    removed tablet checkin from updates controller

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -3,18 +3,8 @@ class UpdatesController < ApplicationController
   before_filter :authenticate_user
 
   def index
-    if params[:tablet]
-      @tablet = Tablet.where(device_id: tablet_params.delete(:device_id)).first_or_initialize
-      @tablet.update_attributes tablet_params
-    end
-
     @class_rooms = current_user.class_rooms
     @updates = current_user.pending_updates params["versions"]
     @cdns = @current_user.school.cdns.where("pinged_at >= ?", 30.minutes.ago).map(&:base_url)
-  end
-
-private
-  def tablet_params
-    @tablet_params ||= params.require(:tablet).permit!
   end
 end
