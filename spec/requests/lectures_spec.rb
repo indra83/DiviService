338aefa5ee23c41d7d7ab408e58139f793062561
Subhash Chandra <TMaYaD@LoonyB.in>
2014-11-06 07:08:59 +0530

    updated getLectures specs

--- spec/requests/lectures_spec.rb
+++ spec/requests/lectures_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 describe "Lecture" do
-  describe "POST /lectures" do
-    let(:class_room) { create :class_room }
-    let(:teacher) { create :user, role: 'teacher', class_rooms: [class_room] }
-    let(:lecture) { build :lecture, teacher: teacher, class_room: class_room }
-    let(:json_payload) { %({"token": "#{teacher.token}", "classRoomId": "#{class_room.id}", "name": "#{lecture.name}" }) }
-    let(:pattern) do
-      {
-        id: :lecture_id,
-        classRoomId: class_room.id.to_s,
-        name: lecture.name,
-        teacherId: teacher.id.to_s,
-        teacherName: teacher.name,
-        channel: /^lecture_\d+$/,
-        startTime: /^\d{13}$/
-      }
-    end
+  let(:class_room) { create :class_room }
+  let(:teacher) { create :user, role: 'teacher', class_rooms: [class_room] }
+  let(:lecture) { build :lecture, teacher: teacher, class_room: class_room }
+  let(:json_payload) { %({"token": "#{teacher.token}", "classRoomId": "#{class_room.id}", "name": "#{lecture.name}" }) }
+  let(:pattern) do
+    {
+      id: :lecture_id,
+      classRoomId: class_room.id.to_s,
+      name: lecture.name,
+      teacherId: teacher.id.to_s,
+      teacherName: teacher.name,
+      channel: /^lecture_\d+$/,
+      startTime: /^\d{13}$/
+    }
+  end
 
+  describe "POST /createLecture" do
     it "should be successfully created by a teacher" do
       post create_lectures_path(format: :json), json_payload, CONTENT_TYPE: 'application/json'
       response.body.should match_json_expression pattern
@@ -39,12 +39,17 @@ describe "Lecture" do
       response.body.should match_json_expression error_pattern
     end
 
+  end
+
+  describe "POST /getLectures" do
     it "should list out all the lectures of a teacher" do
       lecture.save
       post lectures_path(format: :json), %({"token": "#{teacher.token}"}), CONTENT_TYPE: 'application/json'
       response.body.should match_json_expression [pattern]
     end
+  end
 
+  describe "POST /getLectureMembers" do
     it "should list all the members of the lecture to a teacher" do
       lecture.save
       students = create_list :user, 3, class_rooms: [class_room]
