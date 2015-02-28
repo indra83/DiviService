c3eb588f920246f2ded78a67374b3e6693d936bb
Subhash Chandra <TMaYaD@LoonyB.in>
2015-03-01 00:10:10 +0530

    fixed admin pages

--- app/models/tablet.rb
+++ app/models/tablet.rb
@@ -8,9 +8,15 @@ class Tablet < ActiveRecord::Base
 
   belongs_to :user
 
+  def is_content_up_to_date?
+    pending_updates.empty?
+  end
+
   def pending_updates
     user.pending_updates(content['versions'])
   end
+
+  alias_attribute :last_check_in, :updated_at
 
   def timestamps
     content['timestamps'] || Hash.new('n/a')
